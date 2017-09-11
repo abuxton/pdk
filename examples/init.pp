e5ec80e4c7194d591312b86094b37ed58b9e0e77
Adam Buxton <buxton.adam@gmail.com>
2017-09-11 18:23:52 +0100

    tested on RHEL and updated to use staging as source issue with https file source

---
+++ examples/init.pp
@@ -0,0 +1,2 @@
+# simple example
+include pdk
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@
 #
 # Downloads the Puppet Development Toolkit (PDK) based on Fcater facts and version
 # creates a local package file and installs from that file
-# uses /tmp and will isntall as which ever user runs the types, so install local or shared global dependantly
+# uses /tmp and will install as which ever user runs the types, so install local or shared global dependantly
 # @summary installs the PDK
 #
 # @example
@@ -10,18 +10,26 @@
 class pdk(
   String $pdk_version = $pdk::params::pdk_version,
   String $pdk_download_url = $pdk::params::pdk_download_url,
+  String $staging_dir = $pdk::params::staging_dir
   ) inherits pdk::params {
 
-  $pdk_complete_download_url = "https://$pdk_download_url&ver=${pdk_version}"
-  $pdk_local_source = "/tmp/pdk-$pdk_version.${::operatingsystem}${pdk::params::rel}.${pdk::params::pdk_pckg_format}"
+  $pdk_complete_download_url = "https://${pdk_download_url}&ver=${pdk_version}"
+  $pdk_local_pkg = "pdk-${pdk_version}.${::operatingsystem}${pdk::params::rel}.${pdk::params::pdk_pkg_format}"
 
-  file{ $pdk_local_source:
-    ensure => file,
-    source => $pdk_complete_download_url,
+if !defined(Class['staging']){
+  class { 'staging':
+    path  => $staging_dir,
+    #owner => 'puppet',
+    #group => 'puppet',
   }
+}
+staging::file { $pdk_local_pkg :
+  source => $pdk_complete_download_url,
+}
   package {'pdk':
-    ensure => present,
-    source => $pdk_local_source,
-    subscribe  => File[$pdk_local_source],
+    ensure    => present,
+    provider  => $pdk::params::provider,
+    source    => "${staging_dir}/${module_name}/${pdk_local_pkg}",
+    subscribe => Staging::File[$pdk_local_pkg],
   }
 }
