e078420569782e76a41b2fb2168e2d9ba844a095
Adam Buxton <buxton.adam@gmail.com>
2018-09-19 20:35:10 +0100

    update for windows support

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,28 +8,39 @@
 # @example
 #   include pdk
 class pdk(
-  String $pdk_version = $pdk::params::pdk_version,
-  String $pdk_download_url = $pdk::params::pdk_download_url,
-  String $staging_dir = $pdk::params::staging_dir,
+  enum['String','latest','present','absent','purged','installed'] $pdk_version = $pdk::params::pdk_version,
+  Optional[String] $pdk_download_url = $pdk::params::pdk_download_url,
+  Optional[String] $staging_dir = $pdk::params::staging_dir,
   ) inherits pdk::params {
 
-  $pdk_complete_download_url = "https://${pdk_download_url}&ver=${pdk_version}"
-  $pdk_local_pkg = "pdk-${pdk_version}.${::operatingsystem}${pdk::params::rel}.${pdk::params::pdk_pkg_format}"
+    case $facts['operatingsystem'] {
+      'windows' : {
+        package {'pdk':
+        ensure    => $pdk_version,
+        provider  => $pdk::params::provider,
+        source    => $pdk_download_url,
+      }
+    }
+    default: {
+      $pdk_complete_download_url = "https://${pdk_download_url}&ver=${pdk_version}"
+      $pdk_local_pkg = "pdk-${pdk_version}.${::operatingsystem}${pdk::params::rel}.${pdk::params::pdk_pkg_format}"
 
-if !defined(Class['staging']){
-  class { '::staging':
-    path  => $staging_dir,
-    #owner => 'puppet',
-    #group => 'puppet',
-  }
-}
-staging::file { $pdk_local_pkg :
-  source => $pdk_complete_download_url,
-}
-  package {'pdk':
-    ensure    => present,
-    provider  => $pdk::params::provider,
-    source    => "${staging_dir}/${module_name}/${pdk_local_pkg}",
-    subscribe => Staging::File[$pdk_local_pkg],
+      if !defined(Class['staging']){
+        class { '::staging':
+          path  => $staging_dir,
+          #owner => 'puppet',
+          #group => 'puppet',
+        }
+      }
+      staging::file { $pdk_local_pkg :
+        source => $pdk_complete_download_url,
+      }
+      package {'pdk':
+        ensure    => $pdk_version,
+        provider  => $pdk::params::provider,
+        source    => "${staging_dir}/${module_name}/${pdk_local_pkg}",
+        subscribe => Staging::File[$pdk_local_pkg],
+      }
+    }
   }
 }
